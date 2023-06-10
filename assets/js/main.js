cc4b8836e2b9001096ff4adb6a51bfafc00c0598
Fahmiduzzaman Sagar <52732057+turfacreation@users.noreply.github.com>
2023-06-10 19:36:17 +0600

    javascript-practice-class

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+//alert("Allah Mohan");
+
+function student_application_form(){
+    var Select_branch=("Dhaka");
+    var select_academic_year=("2022-2023");
+    var students_application_number=("12345678");
+    var students_name_by_birth_certificate=("Fahmiduzzaman Sagar");
+    var students_date_of_birth=("01-01-1985");
+    var students_gender=("Male");
+    var students_religion=("Islam");
+    var students_nationality=("Bangladeshi");
+    var class_for_applied=("Full Stack Web Development");
+    var students_blood_group=("B(-)");
+    var students_passport_number=("BTX456897");
+    var students_parmanet_address=("Historical Paharpur, Naogaon, Rajshahi");
+    var students_present_address=("Akkelpur, Joypurhat, Rahshahi");
+    var students_fathers_name=("Tozammel Haque");
+    var students_fathers_occupation=("Service Holder");
+    var students_fathers_mobile=("01731778855");
+    var students_father_email=("tozammelhaque@gmail.com");
+    var students_mothers_name=("Fauzia Tunnesha");
+    var students_mothers_occupation=("Service Holder");
+    var students_mothers_mobile=("01732415897");
+    var students_mothers_email=("fauziatunnesa@gmail.com");
+    var students_photo=("photo.jpeg");
+    var students_birth_certificate_image=("certificate.jpeg");
+    var students_living_with=("Both Parents");
+    var payment_transaction_number=("564231B4RE54654");
+    var sender_mobile_number=("01712401190");
+    var application_date=("07-06-2023");
+
+    document.write("<h2>Students Admission Information</h2>");
+
+    document.write("<strong>Select Branch:</strong> "+Select_branch +"<br> <strong>Your Accademic Year:</strong> " + select_academic_year + "<br> <strong>Your Application Number:</strong> " +students_application_number + "<br><strong>Students Name By Birth Certificate:</strong> " +students_name_by_birth_certificate + "<br><strong>Students Date of Birth:</strong> " +students_date_of_birth + "<br><strong>Gender:</strong> " + students_gender + "<br><strong>Religion:</strong> " + students_religion + "<br> <strong>Nationality:</strong>" + students_nationality + "<br><strong>Class For Applied: </strong> "+class_for_applied +"<br><strong>Blood Group:</strong> " + students_blood_group + "<br><strong>Passport Number: </strong> "+students_passport_number+"<br><strong>Parmanent Address: </strong>"+students_parmanet_address+"<br><strong>Present Address:</strong> "+students_present_address+"<br><strong>Fathers Name: </strong> "+students_fathers_name+"<br><strong>Fathers Occupationg: </strong> "+students_fathers_occupation+"<br><strong>Fathers Mobile: </strong> "+students_fathers_mobile+"<br><strong>Fathers Email:</strong> " +students_father_email + "<br><strong>Mothers Name :</strong> " +students_mothers_name + "<br><strong>Mothers Occupation: </strong>" + students_mothers_occupation + "<br><strong>Mothers Mobile: </strong>" + students_mothers_mobile +"<br><strong>Mothers Email: </strong> " + students_mothers_email +"<br><strong>Students Photo: </strong> " + students_photo+ "<br><strong>Students Births Certificate Image: </strong> "+students_birth_certificate_image+"<br><strong>Students Living With: </strong>"+students_living_with+"<br><strong>Payment Transction Number: </strong>" + payment_transaction_number +"<br><strong>Sender Mobile Number: </strong>" +sender_mobile_number +"<br><strong>Applicationd Date: </strong> " +application_date);
+}
+
+
+
+
+
+//"<br> <strong></strong> " + 
